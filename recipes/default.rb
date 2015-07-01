b185749677752fd7f19a367eb9ecd20e7e84703b
Giuseppe Correnti <correnti.g@gmail.com>
2015-07-01 14:49:35 +0200

    Add nginx recipe for graphite-api

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,18 @@ include_recipe "yum-epel" if platform_family?("rhel")
 include_recipe "graphite::whisper"
 include_recipe "graphite::carbon"
 include_recipe "graphite-api"
+
+include_recipe 'nginx'
+
+template '/etc/nginx/sites-available/graphite' do
+  source node['graphite']['nginx']['template']
+  cookbook node['graphite']['nginx']['template_cookbook']
+  notifies :reload, 'service[nginx]'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
+
+nginx_site 'graphite' do
+  template false
+end
